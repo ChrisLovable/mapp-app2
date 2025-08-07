1eeee03a583e6304655c2f263f08365c50bf7190
chrisdevries <chrisdevries.personal@gmail.com>
2025-08-07 18:03:02 +0200

    SIMPLE TEST: Show Landing page only

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Landing from './pages/Landing';
 
 function App() {
-  return <Landing onSelectPlan={plan => { console.log('Selected plan:', plan); }} />;
+  const handleSelectPlan = (plan: 'free' | 'paid') => {
+    alert(`Selected ${plan} plan - this will be wired up next!`);
+  };
+
+  return <Landing onSelectPlan={handleSelectPlan} />;
 }
 
 export default App;
