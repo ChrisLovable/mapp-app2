ca252e451b0c2938ea6ff633afa1ee7361cc71a1
chrisdevries <chrisdevries.personal@gmail.com>
2025-08-08 10:31:17 +0200

    Change number from 2 to 3 at top of main form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -458,7 +458,7 @@ export default function Home({ onShowAuth }: HomeProps) {
       <div className="pt-20 pb-8 px-4">
         {/* Big 1 at the top */}
         <div className="max-w-4xl mx-auto mb-4">
-          <div className="text-white text-6xl font-extrabold text-center">2</div>
+          <div className="text-white text-6xl font-extrabold text-center">3</div>
         </div>
         <Header onDashboardClick={() => {}} onAdminDashboardClick={() => setIsAdminDashboardOpen(true)} />
         
